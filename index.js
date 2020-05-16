f29e21d327a7ee50d1bb25143b653e0e5185b637
Griffin Yourick <gryphon92@gmail.com>
2020-05-16 17:34:46 -0400

    babel test

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-unused-vars */
+const template = require('@babel/template').default;
+const t = require('@babel/types');
 const Benchmark = require('benchmark');
 
 const suite = new Benchmark.Suite();
 
 suite
-  .add('A', () => {
-    const str = 'B';
+  .add('@babel/types factory', () => {
+    const req = t.variableDeclaration('const', [
+      t.variableDeclarator(
+        t.identifier('state.methodsId'),
+        t.callExpression(t.identifier('require'), [
+          t.stringLiteral(`module/lib/methods`),
+        ]),
+      ),
+    ]);
   })
-  .add('B', () => {
-    const str = 'B';
+  .add('@babel/template', () => {
+    const buildRequire = template(`
+      const %%methodsId%% = require(%%methods%%);
+    `);
+
+    const req = buildRequire({
+      methodsId: t.identifier('state.methodsId'),
+      methods: t.stringLiteral(`module/lib/methods`),
+    });
   })
   .on('cycle', function onCycle(event) {
     console.log(event.target.toString());
@@ -15,4 +32,4 @@ suite
   .on('complete', function onComplete() {
     console.log(`Fastest is ${this.filter('fastest').map('name').join(', ')}`);
   })
-  .run({ async: true });
+  .run();
