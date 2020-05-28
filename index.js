a4115fe589cd97873de5f28ffb810199683e872c
Griffin Yourick <gryphon92@gmail.com>
2020-05-28 00:53:02 -0400

    Compare JSON.stringify to v8.serialize

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-unused-vars */
 const Benchmark = require('benchmark');
+const fs = require('fs');
+const v8 = require('v8');
 
 const suite = new Benchmark.Suite();
 
+const obj = {
+  code: fs.readFileSync('./sample.js').toString(),
+};
+
 suite
-  .add('A', () => {
-    const str = 'B';
+  .add('JSON.stringify', () => {
+    JSON.stringify(obj);
   })
-  .add('B', () => {
-    const str = 'B';
+  .add('v8.serialize', () => {
+    v8.serialize(obj);
   })
   .on('cycle', function onCycle(event) {
     console.log(event.target.toString());
