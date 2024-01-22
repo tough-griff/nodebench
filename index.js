addaca5f161bb5ea466ced6bd3d7ab934224682f
Griffin Yourick <gryphon92@gmail.com>
2024-01-22 13:19:19 -0500

    Try out tinybench

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 // @ts-check
-'use strict';
 
-const Benchmark = require('benchmark');
+import { Bench } from 'tinybench';
 
-Benchmark.options.minSamples = 100;
+const bench = new Bench({});
 
-const suite = new Benchmark.Suite();
-
-suite
+bench
   .add('A', () => {
     const str = 'A';
   })
   .add('B', () => {
     const str = 'B';
   })
-  .on('cycle', function onCycle(event) {
-    console.log(event.target.toString());
-  })
-  .on('complete', function onComplete() {
-    console.log(`Fastest is ${this.filter('fastest').map('name').join(', ')}`);
-  })
-  .run({ async: true });
+  .todo('C', () => {
+    const str = 'C';
+  });
+
+await bench.warmup();
+await bench.run();
+
+console.table(bench.table());
