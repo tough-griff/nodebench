81350c8dbbc5df8fecdfd393390343cf2338bbe4
Griffin Yourick <gryphon92@gmail.com>
2024-03-27 13:20:24 -0400

    String#endsWith vs String#match vs path.extname

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 // @ts-check
 
 import { Bench } from 'tinybench';
+import path from 'path';
 
 const bench = new Bench({});
 
+const filename = 'filename.js';
+
 bench
-  .add('A', () => {
-    const str = 'A';
+  .add('.endsWith', () => {
+    filename.endsWith('.mjs');
+    filename.endsWith('.cjs');
   })
-  .add('B', () => {
-    const str = 'B';
+  .add('.match and check', () => {
+    const ext = filename.match(/.[cm]js$/);
+    ext?.[0] === '.mjs';
+    ext?.[0] === '.cjs';
   })
-  .todo('C', () => {
-    const str = 'C';
+  .add('.match x2', () => {
+    filename.match(/.mjs$/);
+    filename.match(/.cjs$/);
+  })
+  .add('path.extname', () => {
+    const ext = path.extname(filename);
+    ext === '.mjs';
+    ext === '.cjs';
   });
 
 await bench.warmup();
